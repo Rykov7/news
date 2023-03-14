5f2f7a6082e71ad0371f0128d1e43d782436b1cc
Rykov7 <rykov7@protonmail.ch>
2023-03-14 09:23:04 +0300

    fix security issues

--- django_project/settings.py
+++ django_project/settings.py
@@ -144,10 +144,10 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-DEFAULT_FROM_EMAIL = "flaskipy@gmail.com"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "flaskipy"
-EMAIL_HOST_PASSWORD = "uzfgnrysdponqukp"
+DEFAULT_FROM_EMAIL = env.str("DEFAULT_FROM_EMAIL")
+EMAIL_HOST = env.str("EMAIL_HOST")
+EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
 
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
